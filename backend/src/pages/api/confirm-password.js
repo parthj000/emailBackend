5f1d14972fdd65274bfc6502c97e104685a753f8
Parth <parsjoshi2@gmail.com>
2024-08-06 12:03:17 +0530

    confirm-password api

---
+++ backend/src/pages/api/confirm-password.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken"
+import clientPromise from "@/lib/mongodb";
+import bcrypt from "bcryptjs";
+
+
+export default async (req,res)=>{
+    if(req.method!=="POST"){
+        return res.status(405).json({ message: "Method not allowed" });
+    }
+
+    const token = req.headers["x-auth-token"];
+    const {password} = req.body;
+    if(!password){
+        return res.status(400).json({message:"All fields are required"})
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const client = await clientPromise;
+      const db = client.db('');
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await db
+        .collection("users")
+        .updateOne(
+          { email: decoded.email },
+          { $set: { password: hashedPassword } }
+        );
+    
+        return res.status(200).json({message:'Password has been set succesfully!'});
+    } catch (err) {
+        console.log(err);
+        return res.send("Something went wrong!");
+    }
+
+
+
+
+}
